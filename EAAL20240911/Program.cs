9ff3bfb26de5a1346db297f685a3baa030f5736e
Albertito21 <815248@clases.edu.sv>
2024-09-12 13:25:29 -0600

    Cambios

--- EAAL20240911/Program.cs
+++ EAAL20240911/Program.cs
@@ -16,28 +16,54 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+// Crear una lista para alamcenar objetos de tipo Marca 
+var marca = new List<Marca>();
 
-app.MapGet("/weatherforecast", () =>
+//Configurar una ruta GET para ontener todas las marcas
+app.MapGet("/marca", () =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+    return marca; // Devuelve la lista de marcas
+});
 
+//Configurar una ruta GET para otener una marca específica por su ID
+app.MapGet("/marca/{id}", (int id) =>
+{
+    //Buscar una marca en la lista que tenga el ID especificado
+    var marcas = marca.FirstOrDefault(c => c.Id == id);
+    return marca; //Devuelve el cliente encontrado (o null si no se encuentra)
+});
+
+//Configurar una ruta POST para agregar una nueva marca a la lista
+app.MapPost("/marca", (Marca marcas) =>
+{
+    marca.Add(marcas); //Agrega la nueva marca a la lista
+    return Results.Ok(); //Devuelve una repuesta HTTP 200 OK
+});
+
+//Configurar una ruta PUT para actualizar una marca existente por su ID
+app.MapPut("/marca/{id}", (int id, Marca marcas) =>
+{
+    //Bsucar una marca en la lista que tenga el ID especificado
+    var existingMarca = marca.FirstOrDefault(c => c.Id == id);
+    if(existingMarca != null)
+    {
+        //Actualiza los datos de la marca existente con los datos proporcionados
+        existingMarca.Nombre = marcas.Nombre;
+        return Results.Ok(); // Devuelve una respuesta HTTP 200 Ok
+    }
+    else
+    {
+        return Results.NotFound(); // Devulve una respuesta HTTP 404 Not Found si la marca no existe
+    }
+});
+
+//Ejecutar la aplicacion
 app.Run();
 
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+//Definicion de la clase Marca que representa la estructura de un cliente
+internal class Marca 
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public int Id { get; set; }
+    public string? Nombre { get; set; }
+
 }
